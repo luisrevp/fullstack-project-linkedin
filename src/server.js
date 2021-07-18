9d1e83509666e5b6f964186c984931d07e95597c
Luis Revilla <luisrev97@gmail.com>
2021-07-18 15:18:03 -0300

    primer commit

---
+++ src/server.js
@@ -0,0 +1,99 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(express.static(path.join(__dirname, "/build")));
+app.use(bodyParser.json());
+
+// esta funcion se encarga de refactorizar todo el codigo empleado abajo (DRY: don't repeat yourself!!!)
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+    });
+    const db = client.db("my-blog");
+
+    await operations(db);
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+};
+
+//////////////////////////////////////////////////////////////
+
+app.get("/api/articles/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
+});
+
+//////////////////////////////////////////////////////////////
+// actualizando la data de UPVOTE
+
+app.post("/api/articles/:name/upvote", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          upvotes: articleInfo.upvotes + 1,
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+//////////////////////////////////////////////////////////////
+// actualizando "ADD-COMMENT"
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+  }, res);
+});
+
+// todas las requests realizadas desde otras API routes deben ser enviadas a nuestra app
+// esto es para que el cliente pueda navegar entre las paginas y URLS que creamos
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+app.listen(8000, () => console.log("Listening on port 8000"));
